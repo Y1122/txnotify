504420f626954c7a7ff157773dfdf2ee4fd73bb6
Will Beebe <willbeebe@gmail.com>
2012-08-26 12:34:20 -0700

    adding blank project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Txnotify::Application.config.secret_token = '38a91e1aa7296762b6e52f5c292b9b482a8565128326d804f293d679dd32188bd1af2349bd73133f3a9e6cedb72752efd60cf02b52f8f64d7e63be5042e34788'
